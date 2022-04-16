63bf07d0871a4df6598b2248c99e1cef59e3e251
王一之 <yz@ggnb.top>
2022-04-16 20:29:11 +0800

    🐛 修复仓库与编辑问题

--- docs/.vuepress/themeConfig.ts
+++ docs/.vuepress/themeConfig.ts
@@ -9,7 +9,11 @@ export default defineThemeConfig({
 
   logo: "/logo.png", // 导航栏图标
 
-  repo: "scriptscat/scriptcat.org", // github 仓库，用于在导航栏中显示仓库链接
+  repo: "scriptscat/scriptcat", // github 仓库，用于在导航栏中显示仓库链接
+
+  docsRepo: "scriptscat/scriptcat.org",
+
+  docsDir: "/docs",
 
   // docsDir: "demo/src", // 文档在仓库中的目录
 
@@ -19,7 +23,8 @@ export default defineThemeConfig({
   // 侧边栏
   sidebar: sidebar,
 
-  footer: "Released under the MIT License Copyright © 2021-2022 ScriptCat 脚本猫", // 页脚
+  footer:
+    "Released under the MIT License Copyright © 2021-2022 ScriptCat 脚本猫", // 页脚
 
   editLink: true, // 是否展示编辑此页链接
 
@@ -27,7 +32,8 @@ export default defineThemeConfig({
 
   pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"], // 文章信息
 
-  themeColor: { // 主题色
+  themeColor: {
+    // 主题色
     blue: "#2196f3",
     skyblue: "#87ceeb",
     red: "#f26d6d",
@@ -42,13 +48,14 @@ export default defineThemeConfig({
   }, */
 
   plugins: {
-
-    copyright: { // 版权信息
+    copyright: {
+      // 版权信息
       global: true,
       author: "ScriptCat",
     },
 
-    search: { // 搜索插件
+    search: {
+      // 搜索插件
       locales: {
         "/": {
           placeholder: "搜索",
@@ -56,7 +63,8 @@ export default defineThemeConfig({
       },
     },
 
-    mdEnhance: { // Markdown 增强功能
+    mdEnhance: {
+      // Markdown 增强功能
       enableAll: true, // 启用全部功能
       presentation: {
         plugins: ["highlight", "math", "search", "notes", "zoom"],
--- docs/about/README.md
+++ docs/about/README.md
@@ -13,3 +13,8 @@ icon: info
 对于我们的开发计划请看:[Project #1](https://github.com/scriptscat/scriptcat/projects/1),我们将在这里公示我们的开发计划,如果你有什么好的意见也可以发[issue](https://github.com/scriptscat/scriptcat/issues)反馈给我们.
 
 **如果觉得好用，顺手点个 Star 吧 ❤❤❤**
+
+## 油猴中文网
+> [点我访问](https://bbs.tampermonkey.net.cn/)
+
+油猴中文网是我们创建的一个油猴脚本的开发与交流的社区。在论坛我们有非常丰富的脚本学习资料和一众大佬，可以在论坛进行交流和向大佬们提问，同时我们也提供脚本的发布和安装，让大家可以自由的分享自己的脚本。
