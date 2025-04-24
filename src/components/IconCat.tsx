5440181944ca14dccda92ac665c3eb8a173e9286
崮生（子虚） <2234839456@qq.com>
2025-04-24 12:14:44 +0000

    新增自定义 footer ，修改首页样式

---
+++ src/components/IconCat.tsx
@@ -0,0 +1,13 @@
+export function IconCat() {
+  return (
+    <img
+      src="/img/logo.png"
+      alt=" Icon"
+      style={{
+        width: '2rem',
+        height: '2rem',
+        color: 'var(--ifm-color-primary)', // Note: This style might not work as expected for images, consider using filter for color change if needed.
+      }}
+    />
+  );
+}
